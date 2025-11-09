dc9aefb459c5939f911b7f984e443c706ab79ae3
faimida-attar <faimida@example.com>
2025-11-09 21:47:52 +0530

    Initial commit

---
+++ frontend/src/pages/About.jsx
@@ -0,0 +1,50 @@
+import React from 'react'
+import Title from '../components/Title'
+import { assets } from '../assets/assets'
+import NewsletterBox from '../components/NewsletterBox'
+
+const About = () => {
+  return (
+    <div>
+
+      <div className='text-2xl text-center pt-8 border-t'>
+          <Title text1={'ABOUT'} text2={'US'} />
+      </div>
+
+      <div className='my-10 flex flex-col md:flex-row gap-16'>
+          <img className='w-full md:max-w-[450px]' src={assets.about_img} alt="" />
+          <div className='flex flex-col justify-center gap-6 md:w-2/4 text-gray-600'>
+              <p>Kolhapuri Chappal was started with love for tradition and a goal to make shopping for handmade chappals easy and enjoyable. Our journey began with a simple thought: to build a website where people can find and buy beautiful Kolhapuri chappals from the comfort of their homes.</p>
+              <p>Since the beginning, we have worked hard to bring together a wide collection of high-quality, stylish, and comfortable chappals. We work with skilled makers to offer original designs that suit men, women, and kids for every occasion.</p>
+              <b className='text-gray-800'>Our Mission</b>
+              <p>Our mission at Kolhapuri Chappal is to give our customers the best choices, comfort, and a smooth shopping experience. From searching for products to placing an order and getting it delivered, we want to make sure everything is easy and satisfying for you.
+              </p>
+          </div>
+      </div>
+
+      <div className=' text-xl py-4'>
+          <Title text1={'WHY'} text2={'CHOOSE US'} />
+      </div>
+
+      <div className='flex flex-col md:flex-row text-sm mb-20'>
+          <div className='border px-10 md:px-16 py-8 sm:py-20 flex flex-col gap-5'>
+            <b>Quality Assurance:</b>
+            <p className=' text-gray-600'>We meticulously select and vet each product to ensure it meets our stringent quality standards.</p>
+          </div>
+          <div className='border px-10 md:px-16 py-8 sm:py-20 flex flex-col gap-5'>
+            <b>Convenience:</b>
+            <p className=' text-gray-600'>With our user-friendly interface and hassle-free ordering process, shopping has never been easier.</p>
+          </div>
+          <div className='border px-10 md:px-16 py-8 sm:py-20 flex flex-col gap-5'>
+            <b>Exceptional Customer Service:</b>
+            <p className=' text-gray-600'>Our team of dedicated professionals is here to assist you the way, ensuring your satisfaction is our top priority.</p>
+          </div>
+      </div>
+
+      <NewsletterBox/>
+      
+    </div>
+  )
+}
+
+export default About
